25eba1d20d72b9a4d9a53632a92257b1fe95a287
Ankit <dante7785@gmail.com>
2021-01-21 21:13:32 +0530

    Questions with Solutions

---
+++ TCS Interview Questions/Split.java
@@ -0,0 +1,15 @@
+public class Split {
+	public static void main(String[] args) {
+		String s1="Sachin Tendulkar";
+		String s2[]=s1.split("l",2);
+		for(int i = 0;i<=s2.length-1;i++){
+		System.out.println(s2[i]);
+			
+	}
+}
+}
+
+// Sachin Tendu
+// kar
+
+
---
+++ TCS Interview Questions/sachin_tendulkar.java
@@ -0,0 +1,13 @@
+public class sachin_tendulkar {
+    public static void main(String[] args) {
+        String s1   = "Sachin Tendulkar";
+        String s2[] = s1.split(" ");
+        for(int i = s2.length-1;i>=0;i--){
+            System.out.println(s2[i]);
+        }
+    }
+    
+}
+
+// Tendulkar
+// Sachin
